fc14c77ebf90b60542b7cf0eb410ae112904c5a6
Umesh R Warrier <37244971+Umesh-R@users.noreply.github.com>
2021-01-10 14:20:20 +0530

    Adding the JS file and the paper lib file

---
+++ js/paperTap.js
@@ -0,0 +1,5 @@
+var myCircle = new Path.Circle(new Point(250, 250), 100);
+	myCircle.fillColor = 'steelblue';
+
+var myCircle1 = new Path.Circle(new Point(100, 70), 50);
+myCircle1.fillColor = 'red';
